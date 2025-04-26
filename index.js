df8cc26e17eab9d2a5ded8a9e12f4746241f05e4
italoesilva <italoliveira95@hotmail.com>
2025-04-26 19:59:07 -0300

    comportamento de classes

---
+++ index.js
@@ -0,0 +1,50 @@
+ class Cliente{
+    nome;
+    cpf;
+    rg;
+ }
+
+
+ class ContaCorrente{
+    agencia;
+    saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+           this.saldo -= valor;
+        }
+        
+    }
+}
+
+
+
+ const cliente1 = new Cliente();
+ cliente1.nome = "Ricardo";
+ cliente1.cpf =11122233309;
+ cliente1.rg = 123456789;
+
+ const cliente2 = new Cliente();
+ cliente2.nome = "Alice";
+ cliente2.cpf =88822255508;
+ cliente2.rg = 3456756522;
+
+ const contaCorrenteRicardo = new ContaCorrente();
+ contaCorrenteRicardo.saldo = 0;
+ contaCorrenteRicardo.agencia = 1001;
+
+
+
+    console.log(contaCorrenteRicardo.saldo);
+    contaCorrenteRicardo.saldo = 100;
+    console.log(contaCorrenteRicardo.saldo);
+    contaCorrenteRicardo.sacar(100)
+
+
+
+
+ console.log(cliente1);
+ console.log(cliente2);
+
+
+
