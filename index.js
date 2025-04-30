77b65d5c292c87150d3b11e23a5b666ec59f9461
italoesilva <italoliveira95@hotmail.com>
2025-04-30 19:36:42 -0300

    package.json/modules/import/export/tipos de valor e referencia

--- index.js
+++ index.js
@@ -6,19 +6,21 @@ import {ContaCorrente} from "./ContaCorrente.js"
  cliente1.cpf =11122233309;
  cliente1.rg = 123456789;
 
- const cliente2 = new Cliente();
- cliente2.nome = "Alice";
- cliente2.cpf =88822255508;
- cliente2.rg = 3456756522;
+// const cliente2 = new Cliente();
+//cliente2.nome = "Alice";
+ //cliente2.cpf =88822255508;
 
- const contaCorrenteRicardo = new ContaCorrente();
- contaCorrenteRicardo.agencia = 1001;
- contaCorrenteRicardo.cliente = cliente1;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = new Cliente();
+conta2.cliente.nome = "Alice";
+conta2.cliente.cpf = 11122233309;
 conta2.agencia = 102;
 
 
@@ -26,7 +28,7 @@ let valor = 200;
 contaCorrenteRicardo.transferir(valor, conta2);
 
 
-console.log("valor: ", valor)
+console.log("valor: ", valor);
 console.log(conta2);
 console.log(contaCorrenteRicardo);
 
